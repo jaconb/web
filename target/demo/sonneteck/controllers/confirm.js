fd495d67f6b495686233d98291d0fee179b723da
sonneteck <974825185@qq.com>
2016-12-12 14:19:23 +0800

    first change

--- target/demo/sonneteck/controllers/confirm.js
+++
@@ -1,9 +0,0 @@
-angular.module('sntApp').controller('ConfirmCtrl',function ($scope, $uibModalInstance, AuthService,$stateParams, toaster, POPTIMEOUT,$rootScope) {
-  
-    $scope.ok = function () {
-        $uibModalInstance.close();
-    };
-    $scope.cancel = function () {
-        $uibModalInstance.dismiss('cancel');
-    };
-})
